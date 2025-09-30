e59079e0589f6672abb6743de363e60c98e3eeea
Zaqpurpur-Neo <baristaunescio@gmail.com>
2025-09-30 10:42:19 +0700

    fix options

--- pages/api/pasien/[nik]/index.ts
+++ pages/api/pasien/[nik]/index.ts
@@ -7,6 +7,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
+  if (req.method === "OPTIONS") {
+  	return res.status(200).end();
+  }
+
   const { nik } = req.query;
   const client = await clientPromise;
   const db = client.db("namanyaapa");
